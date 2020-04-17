1716ee0cd80c1ffcd917b030bb86d27226ffba5b
Francis John Banlasan <francisjohn.banlasan@gmail.com>
2020-04-17 18:54:00 +0800

    Add users create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
+    before_save { self.email = email.downcase }
     has_many :articles
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
-    validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 105 }
+    validates :username, presence: true, 
+                         uniqueness: { case_sensitive: false }, 
+                         length: { minimum: 3, maximum: 25 }
+    validates :email, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      format: { with: VALID_EMAIL_REGEX }, 
+                      length: { maximum: 105 }
+    has_secure_password
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_16_103303) do
+ActiveRecord::Schema.define(version: 2020_04_16_121236) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_04_16_103303) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
