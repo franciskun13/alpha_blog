77fead8843b1abbb8d6d139bd65e3f42c38279c0
Francis John Banlasan <francisjohn.banlasan@gmail.com>
2020-04-26 01:17:13 +0800

    Completed Authentication System

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+
 
     def show
-        @user = User.find(params[:id])
         @articles = @user.articles
     end
 
@@ -14,12 +16,11 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
+
     end
 
     def update
 
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information was updated successfully."
             redirect_to @user
@@ -31,6 +32,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed up"
             redirect_to articles_path
         else
@@ -42,5 +44,17 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def require_same_user
+
+        if  current_user != @user
+            flash[:danger] = "You can only edit or delete your own account"
+            redirect_to root_path
+        end
     end
 end
